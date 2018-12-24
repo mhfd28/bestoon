42eee11150622e23ae28b5d1ac21ace6f8ebf62f
mhfd28 <misterfh2@gmail.com>
2018-12-24 09:01:16 -0500

    requirement freezed

--- web/models.py
+++ web/models.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,3 +9,16 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+
+    def __unicode__ (self):
+        return "{}-{}".format(self.date, self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models. BigIntegerField()
+    user = models.ForeignKey(User)
+
+def __unicode__ (self):
+    return "{}-{}".format(self.date, self.amount)
