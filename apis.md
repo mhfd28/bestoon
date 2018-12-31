a2a4ae7f7d809e4882abc01d0c78db7dca1860ed
root <mehdi farahzadi>
2018-12-31 08:19:42 -0500

    user creation works

---
+++ apis.md
@@ -0,0 +1,9 @@
+/submit/expense/
+  POST,return a jason
+  input: date (optional), text, amount, User
+  output: status:ok
+
+/submit/expense/
+  POST, return a jason
+  input: date (optional), text, amount, User
+  output: status:ok
--- web/models.py
+++ web/models.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length = 120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50) #TODO: do not save password
+
 class Token(models.Model):
     user =  models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=48)
@@ -14,9 +21,8 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
-
-    def __unicode__ (self):
-        return "{}-{}".format(self.date, self.amount)
+    def __unicode__(self):
+        return "{}-{}-{}".format(self.date, self.user , self.amount)
 
 
 class Income(models.Model):
@@ -24,6 +30,5 @@ class Income(models.Model):
     date = models.DateTimeField()
     amount = models. BigIntegerField()
     user = models.ForeignKey(User)
-
-def __unicode__ (self):
-    return "{}-{}".format(self.date, self.amount)
+    def __unicode__ (self):
+        return "{}-{}-{}".format(self.date, self.user, self.amount)
